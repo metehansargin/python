0fd27e8b6702438d9efecfcbb8aca33165f765ed
metehansargin <msargin1986@gmail.com>
2024-01-27 16:02:05 +0300

    update

--- python.py
+++ python.py
@@ -1,5 +1,15 @@
-m="hello world"
-print(m)
 
-number=int(input("Number:"))
-print(number+10)
+"""number=int(input("sayi"))
+if(number<15):
+    print("tuttugun sayi daha dusuk")"""
+import random
+
+print(random.randint(1,6))
+number=int(input("Sayinizi Giriniz:"))
+if(number==random):
+    print("sayiniz random sayiya esit \n")
+"""else:()
+print("sayiniz random sayiya esit degil")"""
+
+
+
