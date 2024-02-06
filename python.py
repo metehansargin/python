055bed9c537dd4a98887c7ed42dadcc7cc4b08e6
metehansargin <msargin1986@gmail.com>
2024-02-06 15:36:55 +0300

    if-else

--- python.py
+++ python.py
@@ -3,13 +3,51 @@
 if(number<15):
     print("tuttugun sayi daha dusuk")"""
 import random
-
+number=int
+sayi=int
+sayi=random
+"""
 print(random.randint(1,6))
-number=int(input("Sayinizi Giriniz:"))
-if(number==random):
+number=input("Sayinizi Giriniz:")
+if number == sayi:
     print("sayiniz random sayiya esit \n")
-"""else:()
-print("sayiniz random sayiya esit degil")"""
+
+else:
+    print("sayiniz random sayidan kucuk")"""
+
+drivers_lience=True
+age=18
+
+if age>17:
+    if drivers_lience:
+        print("you can driver car")
+    else:
+        print("you need to go to a  drivers  licence course")
+
+else:
+    print("youu need  to get older")
 
 
+village_name="arifegazili"
+
+for i in village_name:
+    print(i)
+print("complated")
+
+for a in range(1,10):
+    if a==9:
+       print("Sayiniz 9",a)
+    else:
+     print(a)
+
+for b in range(1,101):
+  if  b%5==0 and b%2==0:
+   continue
+  elif  b%5==0:
+    print(b)
+  elif b%2==0:
+    print(b)
+   
+       
+ 
 
